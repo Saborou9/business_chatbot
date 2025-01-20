92dcbcc877ac424fb3d8e5aff7234a6410526d78
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-01-20 17:51:10 +0100

    chore: update Tailwind config with new content paths and color definitions

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,15 +2,25 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
+    './app/views/**/*.{erb,html}',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/assets/stylesheets/**/*.css',
+    './app/components/**/*.{rb,erb,html}'
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        green: {
+          500: '#22c55e',
+          400: '#4ade80',
+        },
+        gray: {
+          300: '#d1d5db',
+        },
       },
     },
   },
