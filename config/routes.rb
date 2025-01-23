24ffbf471f21aa067c70948c752a9c7d9b7e3e78
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-01-23 19:23:28 +0100

    feat: integrate Devise for user authentication and update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
   root "pages#home"
   get '/about', to: 'pages#about', as: :about
   get '/pricing', to: 'pages#pricing', as: :pricing
   get '/signup', to: 'pages#signup', as: :signup
+  get '/login', to: 'pages#login', as: :login
 end
