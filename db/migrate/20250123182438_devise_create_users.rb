9fe123e52d241fc75c0c8e98e6a69b1ef8a84e09
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-01-29 14:40:11 +0100

    refactor: Modify Devise migration to conditionally add columns to existing users table

--- db/migrate/20250123182438_devise_create_users.rb
+++ db/migrate/20250123182438_devise_create_users.rb
@@ -2,43 +2,57 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+    # Remove create_table block
+    # Add or modify columns conditionally
+    
+    # Authenticatable
+    change_table :users do |t|
+      unless column_exists?(:users, :email)
+        t.string :email, null: false, default: ""
+      end
+      
+      unless column_exists?(:users, :encrypted_password)
+        t.string :encrypted_password, null: false, default: ""
+      end
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # Recoverable
+      unless column_exists?(:users, :reset_password_token)
+        t.string :reset_password_token
+      end
+      
+      unless column_exists?(:users, :reset_password_sent_at)
+        t.datetime :reset_password_sent_at
+      end
 
-      ## Rememberable
-      t.datetime :remember_created_at
+      # Rememberable
+      unless column_exists?(:users, :remember_created_at)
+        t.datetime :remember_created_at
+      end
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
-
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-
-      t.timestamps null: false
+      # Optional: Trackable
+      unless column_exists?(:users, :sign_in_count)
+        t.integer :sign_in_count, default: 0, null: false
+      end
+      
+      unless column_exists?(:users, :current_sign_in_at)
+        t.datetime :current_sign_in_at
+      end
+      
+      unless column_exists?(:users, :last_sign_in_at)
+        t.datetime :last_sign_in_at
+      end
+      
+      unless column_exists?(:users, :current_sign_in_ip)
+        t.string :current_sign_in_ip
+      end
+      
+      unless column_exists?(:users, :last_sign_in_ip)
+        t.string :last_sign_in_ip
+      end
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # Add indexes if they don't exist
+    add_index :users, :email, unique: true unless index_exists?(:users, :email)
+    add_index :users, :reset_password_token, unique: true unless index_exists?(:users, :reset_password_token)
   end
 end
