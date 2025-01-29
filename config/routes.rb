16f1c4b223619c7e79ca01f3ab023b1da73f6725
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-01-29 15:10:24 +0100

    feat: Implement Devise login view with Tailwind styling and remove legacy login/signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get '/about', to: 'pages#about', as: :about
   get '/pricing', to: 'pages#pricing', as: :pricing
-  get '/signup', to: 'pages#signup', as: :signup
-  get '/login', to: 'pages#login', as: :login
-  
   # Chat routes
   resources :chat, only: [:index, :create]
 end
