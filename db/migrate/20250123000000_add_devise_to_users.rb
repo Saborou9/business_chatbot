1970694fc880d4ffddd6e63ad088710dba82e056
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-01-29 14:30:55 +0100

    refactor: Update Devise migration with safe column and index additions

--- db/migrate/20250123000000_add_devise_to_users.rb
+++ db/migrate/20250123000000_add_devise_to_users.rb
@@ -1,48 +1,24 @@
 class AddDeviseToUsers < ActiveRecord::Migration[8.0]
   def self.up
-    change_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
-
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
+    change_table :users, bulk: true do |t|
+      # Use if_not_exists: true for each column
+      t.string :email, null: false, default: "", if_not_exists: true
+      t.string :encrypted_password, null: false, default: "", if_not_exists: true
+      t.string :reset_password_token, if_not_exists: true
+      t.datetime :reset_password_sent_at, if_not_exists: true
+      t.datetime :remember_created_at, if_not_exists: true
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # Use safe_add_index with if_not_exists: true
+    add_index :users, :email, unique: true, if_not_exists: true
+    add_index :users, :reset_password_token, unique: true, if_not_exists: true
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    # Provide a safe way to rollback
+    remove_index :users, :email if index_exists?(:users, :email)
+    remove_index :users, :reset_password_token if index_exists?(:users, :reset_password_token)
+    
+    remove_columns :users, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   end
 end
