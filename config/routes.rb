b9b78172da2beb71b385248cbabbca0832d4d86a
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-02-07 12:19:15 +0100

    feat: Configure Devise to use custom registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :messages
-  devise_for :users, sign_out_via: [ :get, :delete ]
+  devise_for :users, 
+    sign_out_via: [ :get, :delete ],
+    controllers: { 
+      registrations: 'registrations' 
+    }
   resources :chats, except: [ :edit, :update ]
   get "/about", to: "pages#about", as: :about
   get "/pricing", to: "pages#pricing", as: :pricing
