d5e2250b0a3ec06ccb5c8e11e23e76965ce94a69
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-02-07 14:28:26 +0100

    feat: Add message creation with user association and validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,70 +1,28 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_chat
 
-  # GET /messages or /messages.json
-  def index
-    @messages = Message.all
-  end
-
-  # GET /messages/1 or /messages/1.json
-  def show
-  end
-
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
-
-  # GET /messages/1/edit
-  def edit
-  end
-
-  # POST /messages or /messages.json
   def create
-    @message = Message.new(message_params)
+    @message = @chat.messages.new(message_params.merge(user: current_user))
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
-        format.json { render :show, status: :created, location: @message }
+        format.turbo_stream
+        format.html { redirect_to @chat }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@message, partial: "messages/form", locals: { chat: @chat, message: @message }) }
+        format.html { render 'chats/show', status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /messages/1 or /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: "Message was successfully updated." }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1 or /messages/1.json
-  def destroy
-    @message.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to messages_path, status: :see_other, notice: "Message was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def message_params
-      params.expect(message: [ :chat_id ])
-    end
+  def set_chat
+    @chat = current_user.chats.find(params[:chat_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :chat
+  belongs_to :user
+
+  validates :content, presence: true
 end
