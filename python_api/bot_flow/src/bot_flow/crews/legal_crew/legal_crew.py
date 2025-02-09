4eefc1d27d75e296c7917d511b695b671218b4ef
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-02-09 19:37:12 +0100

    feat: Add Legal Crew with agent and task configurations for legal guidance

---
+++ python_api/bot_flow/src/bot_flow/crews/legal_crew/legal_crew.py
@@ -0,0 +1,56 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+
+from python_api.bot_flow.src.bot_flow.shared_utils.model_utils import get_model_identifier, get_model_api_key
+
+@CrewBase
+class LegalCrew:
+    """Poem Crew"""
+
+    def __init__(
+        self,
+        show_logs=False,
+        model_name="4o-mini",
+        directory="",
+    ):
+        self.show_logs = show_logs
+        self.model_name = model_name
+        self.directory = directory
+
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def legal_compliance_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["legal_compliance_agent"],
+            llm=LLM(
+                model=get_model_identifier(self.model_name),
+                api_key=os.getenv(get_model_api_key(self.model_name)),
+                max_tokens=8192,
+                temperature=0.1,
+                top_p=0.65,
+                presence_penalty=0.1,
+                frequency_penalty=0.4,
+            ),
+            verbose=self.show_logs,
+            cache=False
+        )
+
+    @task
+    def provide_legal_guidance_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["provide_legal_guidance"],
+            agent=self.legal_compliance_agent()
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Research Crew"""
+        
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
