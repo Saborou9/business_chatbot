11545d9e28a3cf8eeba5084bf1194bf09e7d96e9
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-02-11 16:28:44 +0100

    chore: Update .gitignore and change model_name in test.py to 'sonnet'

--- python_api/bot_flow/test.py
+++ python_api/bot_flow/test.py
@@ -4,7 +4,7 @@ BuddyFlow(
     question="What are the latest trends in AI for small businesses?",
     directory="10_2025_01_23_21_10_54",
     show_logs=False,
-    model_name="4o-mini",
+    model_name="sonnet",
     search_timeframe="d",
     search_results=10,
     search_results_parsed=2,
