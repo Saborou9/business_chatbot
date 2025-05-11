0f8bd087c837e6396d79c2f7b1079f6df1434b48
Saborou9 <69769035+Saborou9@users.noreply.github.com>
2025-05-11 20:03:25 +0200

    feat: implement turbo frame modal for new chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,77 +1,29 @@
 class ChatsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_chat, only: %i[ show destroy update_preferred_model ]
-
-  # GET /chats or /chats.json
-  def index
-    @chats = current_user.chats
-  end
-
-  # GET /chats/1 or /chats/1.json
-  def show
-    @chats = current_user.chats
-    respond_to do |format|
-      format.html { render :show }
-      format.turbo_stream
-    end
-  end
-
+  
   # GET /chats/new
   def new
     @chat = current_user.chats.new
+    respond_to do |format|
+      format.html # Renders new.html.erb
+      format.turbo_stream # Renders new.turbo_stream.erb
+    end
   end
 
-  # POST /chats or /chats.json
+  # POST /chats
   def create
     @chat = current_user.chats.new(chat_params)
-
-    respond_to do |format|
-      if @chat.save
-        format.html { redirect_to @chat, notice: "Chat was successfully created." }
-        format.json { render :show, status: :created, location: @chat }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @chat.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /chats/1 or /chats/1.json
-  def destroy
-    @chat.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to chats_path, status: :see_other, notice: "Chat was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  def update_preferred_model
-    Rails.logger.debug "Received params: #{params.inspect}"
-
-    if @chat.update(params.require(:chat).permit(:preferred_model))
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("chat_content", partial: "chats/chat_content", locals: { chat: @chat }) }
-        format.html { redirect_to chat_path(@chat), notice: "Model updated." }
-      end
+    
+    if @chat.save
+      redirect_to @chat, notice: "Chat created successfully"
     else
-      Rails.logger.error "Failed to update model: #{@chat.errors.full_messages.join(', ')}"
-
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("chat_content", partial: "chats/chat_content", locals: { chat: @chat }) }
-        format.html { redirect_to chat_path(@chat), alert: "Failed to update model." }
-      end
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_chat
-      @chat = current_user.chats.find(params.require(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def chat_params
-      params.require(:chat).permit(:title, :preferred_model)
-    end
+  def chat_params
+    params.require(:chat).permit(:title, :preferred_model)
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,17 +2,6 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-// Debug before registration
-console.log("Registering modal controller...")
-
-// Register controller
-import { application } from "controllers/application"
-import ModalController from "./controllers/modal_controller"
-application.register("modal", ModalController)
-
-// Debug after registration
-console.log("Registered controllers:", Object.keys(application.controllers))
-
 // Eager load all controllers
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
